80e1bca8271bb691f56315fe78e1f314faf054ad
George Nakas <george.nakas@mynxg.com>
2021-09-14 13:59:25 +0200

    creating first model

---
+++ accelecation_test.py
@@ -0,0 +1,205 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+filename = "content/punch.csv"
+
+df = pd.read_csv(filename)
+print(df.shape)
+
+index = range(1, len(df['aX']) + 1)
+
+plt.rcParams["figure.figsize"] = (20,7)
+
+plt.plot(index, df['aX'], 'g.', label='x', linestyle='solid', marker=',')
+plt.plot(index, df['aY'], 'b.', label='y', linestyle='solid', marker=',')
+plt.plot(index, df['aZ'], 'r.', label='z', linestyle='solid', marker=',')
+plt.title("Punch")
+plt.xlabel("Sample #")
+plt.ylabel("Acceleration (G)")
+plt.legend()
+# plt.show()
+
+filename = "content/flex.csv"
+
+df = pd.read_csv(filename)
+
+index = range(1, len(df['aX']) + 1)
+
+plt.rcParams["figure.figsize"] = (20,10)
+
+plt.plot(index, df['aX'], 'g.', label='x', linestyle='solid', marker=',')
+plt.plot(index, df['aY'], 'b.', label='y', linestyle='solid', marker=',')
+plt.plot(index, df['aZ'], 'r.', label='z', linestyle='solid', marker=',')
+plt.title("flex")
+plt.xlabel("Sample #")
+plt.ylabel("Acceleration (G)")
+plt.legend()
+# plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+print(f"TensorFlow version = {tf.__version__}\n")
+
+# Set a fixed random seed value, for reproducibility, this will allow us to get
+# the same random numbers each time the notebook is run
+SEED = 1337
+np.random.seed(SEED)
+tf.random.set_seed(SEED)
+
+# the list of gestures that data is available for
+GESTURES = [
+    "punch",
+    "flex",
+]
+
+SAMPLES_PER_GESTURE = 119
+
+NUM_GESTURES = len(GESTURES)
+
+# create a one-hot encoded matrix that is used in the output
+ONE_HOT_ENCODED_GESTURES = np.eye(NUM_GESTURES)
+
+inputs = []
+outputs = []
+
+# read each csv file and push an input and output
+for gesture_index in range(NUM_GESTURES):
+  gesture = GESTURES[gesture_index]
+  print(f"Processing index {gesture_index} for gesture '{gesture}'.")
+  
+  output = ONE_HOT_ENCODED_GESTURES[gesture_index]
+  
+  df = pd.read_csv("content/" + gesture + ".csv")
+  
+  # calculate the number of gesture recordings in the file
+  num_recordings = int(df.shape[0] / SAMPLES_PER_GESTURE)
+  
+  print(f"\tThere are {num_recordings} recordings of the {gesture} gesture.")
+  
+  for i in range(num_recordings):
+    tensor = []
+    for j in range(SAMPLES_PER_GESTURE):
+      index = i * SAMPLES_PER_GESTURE + j
+      # normalize the input data, between 0 to 1:
+      # - acceleration is between: -4 to +4
+      # - gyroscope is between: -2000 to +2000
+      tensor += [
+          (df['aX'][index] + 4) / 8,
+          (df['aY'][index] + 4) / 8,
+          (df['aZ'][index] + 4) / 8,
+          (df['gX'][index] + 2000) / 4000,
+          (df['gY'][index] + 2000) / 4000,
+          (df['gZ'][index] + 2000) / 4000
+      ]
+
+    inputs.append(tensor)
+    outputs.append(output)
+
+# convert the list to numpy array
+inputs = np.array(inputs)
+outputs = np.array(outputs)
+
+print("Data set parsing and preparation complete.")
+
+
+# Randomize the order of the inputs, so they can be evenly distributed for training, testing, and validation
+# https://stackoverflow.com/a/37710486/2020087
+num_inputs = len(inputs)
+randomize = np.arange(num_inputs)
+np.random.shuffle(randomize)
+
+# Swap the consecutive indexes (0, 1, 2, etc) with the randomized indexes
+inputs = inputs[randomize]
+outputs = outputs[randomize]
+
+# Split the recordings (group of samples) into three sets: training, testing and validation
+TRAIN_SPLIT = int(0.6 * num_inputs)
+TEST_SPLIT = int(0.2 * num_inputs + TRAIN_SPLIT)
+
+inputs_train, inputs_test, inputs_validate = np.split(inputs, [TRAIN_SPLIT, TEST_SPLIT])
+outputs_train, outputs_test, outputs_validate = np.split(outputs, [TRAIN_SPLIT, TEST_SPLIT])
+
+print("Data set randomization and splitting complete.")
+
+inputs_train.shape
+
+# build the model and train it
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(50, activation='relu')) # relu is used for performance
+model.add(tf.keras.layers.Dense(15, activation='relu'))
+model.add(tf.keras.layers.Dense(NUM_GESTURES, activation='softmax')) # softmax is used, because we only expect one gesture to occur per input
+model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+
+history = model.fit(inputs_train, outputs_train, epochs=200, batch_size=1, validation_data=(inputs_validate, outputs_validate))
+
+# increase the size of the graphs. The default size is (6,4).
+#plt.rcParams["figure.figsize"] = (20,10)
+
+# graph the loss, the model above is configure to use "mean squared error" as the loss function
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'g.', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+# plt.show()
+
+print(plt.rcParams["figure.figsize"])
+
+# graph the loss again skipping a bit of the start
+SKIP = 100
+plt.plot(epochs[SKIP:], loss[SKIP:], 'g.', label='Training loss')
+plt.plot(epochs[SKIP:], val_loss[SKIP:], 'b.', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+# plt.show()
+
+
+# graph of mean absolute error
+mae = history.history['mae']
+val_mae = history.history['val_mae']
+plt.plot(epochs[SKIP:], mae[SKIP:], 'g.', label='Training MAE')
+plt.plot(epochs[SKIP:], val_mae[SKIP:], 'b.', label='Validation MAE')
+plt.title('Training and validation mean absolute error')
+plt.xlabel('Epochs')
+plt.ylabel('MAE')
+plt.legend()
+# plt.show()
+
+# use the model to predict the test inputs
+predictions = model.predict(inputs_test)
+
+# print the predictions and the expected ouputs
+print("predictions =\n", np.round(predictions, decimals=3))
+print("actual =\n", outputs_test)
+
+# Plot the predictions along with to the test data
+plt.clf()
+plt.title('Training data predicted vs actual values')
+plt.plot(predictions, outputs_test, 'g.', label='Predicted -Actual')
+plt.show()
+
+# Convert the model to the TensorFlow Lite format without quantization
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the model to disk
+open("deingena_model.tflite", "wb").write(tflite_model)
+  
+import os
+basic_model_size = os.path.getsize("deingena_model.tflite")
+print("Model is %d bytes" % basic_model_size)
+
+os.system("echo \"const unsigned char model[] = {\" > content/deingena_model.h")
+os.system("cat deingena_model.tflite | xxd -i      >> content/deingena_model.h")
+os.system("echo \"};\"                             >> content/deingena_model.h")
